7e9d16458e74d4a27df06f827e8d298ec59e38a5
Ashley <grigsby.ashley@gmail.com>
2017-10-16 16:20:15 -0400

    broken vue sorting hat

--- src/main.js
+++ src/main.js
@@ -1,7 +1,73 @@
-import Vue from 'vue'
-import App from './App.vue'
+var sorting_hat = new Vue ({
 
-new Vue({
-  el: '#app',
-  render: h => h(App)
+
+	el: '#quiz',
+
+	data: {
+
+		// responses: [
+
+  //   { 
+  //     text: 'What is your favorite color?',
+  //     answers: [
+  //       {
+  //         id: 1,
+  //         text: 'Scarlet',
+  //         house: 'Gryffindor'
+  //       },
+  //       {
+  //         id: 2,
+  //         text: 'Yellow',
+  //         house: 'Hufflepuff'
+  //       },
+  //       {
+  //         id: 3,
+  //         text: 'Blue',
+  //         house: 'Ravenclaw'
+  //       },
+  //       {
+  //         id: 4,
+  //         text: 'Green',
+  //         house: 'Slytherin'
+  //       }
+  //     ]
+  //   },
+
+
+		// ]
+	}
 })
+
+console.log(data)
+
+// Vue.component('question-list', {
+// 	template: '<div class="question-list">\
+// 				<p>{{question}}</p>\
+// 				<ul>\
+// 					<li v-for="question in questions">\
+// 						<p><strong>{{questions.text}}</strong></p>\
+// 						<p>{{answer.text}}</p>\
+// 						<p><strong>{{house}}</strong></p>\
+// 						</li>\
+// 					</ul>\
+// 				</div>',
+// 	props: ['question', 'questions']
+// 	});
+
+
+
+
+var vm = new Vue({
+  el: '#quiz',
+  data: {
+  		theQuestions: []
+  },
+
+  // created: function() {
+  // 		$.getScript('data.js')
+  // 			.done(function(data) {
+  // 				vm.theQuestions = data;
+  // 			})
+  // }
+
+});
